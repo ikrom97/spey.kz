525ccc27dc0906393ab3d16ca0f1ad6b4e7d103e
ikrom97 <fleck97rgb@gmail.com>
2021-12-11 15:26:52 +0500

    products categories section responsived

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -92,7 +92,9 @@
                                     <span class="view-more__icon">
                                         <svg xmlns="http://www.w3.org/2000/svg" width="8" height="14.525" viewBox="0 0 8 14.525"><g transform="translate(0 14.525) rotate(-90)"><path d="M13.276.216,7.263,6.229,1.251.215A.733.733,0,0,0,.214,1.253l6.53,6.532h0a.732.732,0,0,0,1.036,0l6.53-6.532A.733.733,0,1,0,13.276.216Z" fill="#fff"></path></g></svg>
                                     </span>
-                                    {{__('View products')}}
+                                    <span class="view-more__text">
+                                        {{__('View products')}}
+                                    </span>
                                 </div>
                             </a>
                         </li>
